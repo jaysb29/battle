a58d46de6b4751dfc0f712c9e669459374563702
Jayshree <jbhongle@splunk.com>
2018-11-30 10:38:06 +0530

    did formatting

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-'secret': 'instaassign',
-'database': 'mongodb://instar:Instarem1@ds047438.mlab.com:47438/battle'
+	'secret': 'instaassign',
+	'database': 'mongodb://instar:Instarem1@ds047438.mlab.com:47438/battle'
 }
